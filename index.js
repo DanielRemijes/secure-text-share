fddc8f7333a26101fd2b121de1614f15039875a5
DanielRemijes <danielremijes2002@gmail.com>
2026-05-23 10:30:13 +0530

    fix static file serving

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ app.delete('/api/texts/:id', async (req, res) => {
   res.json({ message: 'Deleted' });
 });
 
+// Serve frontend
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
